66e8008ad284494c7c4da95487df8e6b0881e032
Yogendra Jaiswal <64101128+yogendra-j@users.noreply.github.com>
2020-07-08 05:46:52 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//select DOM items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+//Set Inition State
+let showMenu = false;
+
+const toggleMenu = () => {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    for (item of navItems) {
+      item.classList.add("show");
+    }
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    for (item of navItems) {
+      item.classList.remove("show");
+    }
+    showMenu = false;
+  }
+};
+menuBtn.addEventListener("click", toggleMenu);
